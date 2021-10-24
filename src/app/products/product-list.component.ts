79d6a53224b1b65b19fcaaf3aec34e34cd4e91f9
Borja Gonzalez <bgonzalezferreiro@gmail.com>
2021-10-24 11:09:53 +0200

    Routing (Chapter 10)

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,53 +1,47 @@
-import { Component, OnInit } from "@angular/core";
-import { IProduct } from "./IProduct";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {ProductService} from "./product.service";
+import {IProduct} from "./iproduct";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html'
 })
 
-export class ProductListComponent  implements OnInit{
+export class ProductListComponent  implements OnInit, OnDestroy{
   pageTitle : string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage : boolean = true;
 
   private _prodFilter: string = '';
+  private sub!: Subscription;
+  private errorMessage: string = '';
+
+  constructor(private productService: ProductService) {}
+
   public get prodFilter(): string {
     return this._prodFilter;
   }
   public set prodFilter(value: string) {
     this._prodFilter = value;
-    this.productsFiltered = this.applyFilter(this._prodFilter);
+    this.filteredProducts = this.applyFilter(this._prodFilter);
 
   }
-  productsFiltered : IProduct[] = [];
+  filteredProducts : IProduct[] = [];
 
-  productList : IProduct[] = [
-    {
-      productId: 2,
-      productName: "Davidson",
-      productCode: "DAV-EST-DIGIT",
-      releaseDate: "2021-08-06",
-      price: 4.5,
-      description: "This is a fake",
-      starRating: 4,
-      imageUrl: "assets/images/xbox-controller.png",
-    },
-    {
-      productId: 4,
-      productName: "Coupa",
-      productCode: "COUP-IS-Past",
-      releaseDate: "2021-01-01",
-      price: 1.5,
-      description: "This was a fake",
-      starRating: 1,
-      imageUrl: "assets/images/hammer.png",
-    }
-  ];
+  products : IProduct[] = [];
 
   ngOnInit(): void {
-    this.prodFilter = "dav";
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   onRatingClicked(message: string): void{
@@ -60,6 +54,6 @@ export class ProductListComponent  implements OnInit{
 
   applyFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLowerCase();
-    return this.productList.filter(prod => prod.productName.toLowerCase().includes(filterBy) );
+    return this.products.filter(prod => prod.productName.toLowerCase().includes(filterBy) );
   }
 }
